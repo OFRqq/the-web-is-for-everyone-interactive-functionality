dd348abbd7c0891b7736a05bd755c26bf6988cf5
OFRqq <ole.franssen@gmail.com>
2025-04-02 13:50:28 +0200

    #1 #7 #4 #5 #9 (style error page) (playlist POST) (detail page) (forloop changes)

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ import { Liquid } from "liquidjs";
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express();
 
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+
+
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static("public"));
@@ -31,28 +35,85 @@ app.engine("liquid", engine.express());
 app.set("views", "./views");
 
 // ophalen van stories, audio en buddy data
-const storyResponse = await fetch(`https://fdnd-agency.directus.app/items/tm_story?fields=*.*`); 
-const storyResponseJSON = await storyResponse.json(); 
+const storyResponse = await fetch(
+  `https://fdnd-agency.directus.app/items/tm_story?fields=*.*`
+);
+const storyResponseJSON = await storyResponse.json();
 
-const audioResponse = await fetch(`https://fdnd-agency.directus.app/items/tm_audio`);
+const audioResponse = await fetch(
+  `https://fdnd-agency.directus.app/items/tm_audio`
+);
 const audioResponseJSON = await audioResponse.json();
 
-const buddyResponse = await fetch(`https://fdnd-agency.directus.app/items/tm_buddy`); 
+const buddyResponse = await fetch(
+  `https://fdnd-agency.directus.app/items/tm_buddy`
+);
 const buddyResponseJSON = await buddyResponse.json();
 
- // inladen van de index pagina met de story en audio data
-app.get('/', async function (request, response) { 
- 
-  response.render('index.liquid', {
+const playlistResponse = await fetch(
+  "https://fdnd-agency.directus.app/items/tm_playlist"
+);
+const playlistResponseJSON = await playlistResponse.json();
+// inladen van de index pagina met de story en audio data
+
+
+app.get("/", async function (request, response) {
+
+  response.render("index.liquid", {
     story: storyResponseJSON.data,
     audio: audioResponseJSON.data,
   });
-})
+});
 
-app.get("/story", async function (request, response) {
+app.get("/storydetail/:id", async function (request, response) {
   // Render index.liquid uit de Views map
   // Geef hier eventueel data aan mee
-  response.render("detail.liquid");
+  const storydetailResponse = await fetch(`https://fdnd-agency.directus.app/items/tm_story/` + request.params.id);
+  const storydetailResponseJSON = await storydetailResponse.json();
+
+ 
+  response.render("storydetail.liquid", {
+    storydetail: storydetailResponseJSON.data,
+  });
+});
+
+app.post('/playlist', async function (request, response) {
+  // console.log(request.body)
+  // eerst fetch ik naar de juiste informatie
+  const results = await fetch('https://fdnd-agency.directus.app/items/tm_playlist',{
+    method: 'POST',
+    body: JSON.stringify({
+      title: request.body.title,
+      description: request.body.description,
+    }),
+    headers: {
+      'Content-type':'application/json;charset=UTF-8'
+    }
+  });
+  console.log(results)
+  response.redirect(303, '/')
+})
+
+app.post('/delete/:id', async function (request, response) {
+  // console.log(request.body)
+
+  const deleteplaylist = await fetch(`https://fdnd-agency.directus.app/items/tm_playlist`,{
+    method: 'DELETE',
+
+  });
+  // console.log(deleteplaylist)
+
+  response.redirect(303, '/')
+})
+
+app.get("/playlist", async function (request, response) {
+
+  // Render index.liquid uit de Views map
+  // Geef hier eventueel data aan mee
+  response.render("playlist.liquid", {
+    playlist: playlistResponseJSON.data,
+    story: storyResponseJSON.data,
+  });
 });
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
@@ -64,8 +125,8 @@ app.post("/", async function (request, response) {
 });
 
 app.use((req, res, next) => {
-res.status(404).render("error.liquid")
-})
+  res.status(404).render("error.liquid");
+});
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set("port", process.env.PORT || 8000);
@@ -75,4 +136,3 @@ app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
-
